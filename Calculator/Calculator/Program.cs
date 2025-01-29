74eb6c5e8e354c81d36872423baf9e8bfdf94295
Ruslan <rkupervas2003@gmail.com>
2025-01-29 12:32:10 +0200

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введіть перше число");
+double num1 = Convert.ToDouble((Console.ReadLine()));
+Console.WriteLine("Виберіть опцію + - * /");
+char op = Convert.ToChar((Console.ReadLine()));
+Console.WriteLine("Введіть друге число");
+double num2 = Convert.ToDouble((Console.ReadLine()));
+double result = 0;
+switch (op)
+{
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 == 0)
+        {
+            Console.WriteLine("Ділити на нуль не можна!");
+            return;
+        }
+        result = num1 / num2;
+        break;
+}
+Console.WriteLine($"Відповідь: {result}");
