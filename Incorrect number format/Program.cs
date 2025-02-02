88e8d0d2e39a34986d162c8a2d66d53ab5c7e2ce
Ruslan <rkupervas2003@gmail.com>
2025-02-02 11:03:48 +0200

    Add files via upload

---
+++ Incorrect number format/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введіть число");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine($"Ваше число {number}");
+}
+else
+{
+    Console.WriteLine("Ваше значення не є числом!");
+}
