9c60ccec92a504389e9fe23bfc5902a7a0785017
Ruslan <rkupervas2003@gmail.com>
2025-02-02 10:42:58 +0200

    Add files via upload

---
+++ Division/Program.cs
@@ -0,0 +1,18 @@
+﻿int user_write, user_write2;
+double result = 0;
+Console.WriteLine("Введіть перше число:");
+user_write = int.Parse(Console.ReadLine());
+do
+{
+    Console.WriteLine("Введіть друге число:");
+    user_write2 = int.Parse(Console.ReadLine());
+    if (user_write2 == 0)
+        Console.WriteLine("Ділити на нуль не можна, введіть інше число!");
+    else
+    {
+        result = user_write / user_write2;
+    }
+}
+
+while (user_write2 ==0);
+Console.WriteLine($"Результат:{result}");
