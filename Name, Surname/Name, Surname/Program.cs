9c835f8582d48bc6e96fd368cb51d2d7e821470e
Ruslan <rkupervas2003@gmail.com>
2025-02-21 12:07:58 +0200

    Add files via upload

---
+++ Name, Surname/Name, Surname/Program.cs
@@ -0,0 +1,14 @@
+﻿string name = "Ruslan";
+Console.WriteLine($"name = {name}");
+
+string surname = "Rupervas";
+Console.WriteLine($"surname = {surname}");
+
+if (name[0] == surname[0])
+{
+    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я.");
+}
+else
+{
+    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я.");
+}
