1888acba72077c7e430a6bef6972b2af7cf355af
Ruslan <rkupervas2003@gmail.com>
2025-03-01 00:16:52 +0200

    Add files via upload

---
+++ Fibonacci numbers/Fibonacci numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Channels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введіть число: ");
+        int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+        int result = Fibonacci.getFibonacci(inputNumber);
+        Console.WriteLine($"Число Фібоначчі для {inputNumber}: {result} ");
+    }
+}
+public static class Fibonacci
+{
+    public static int getFibonacci(int number)
+    {
+        if (number == 0)
+        {
+            return 0;
+        }
+        else if (number == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return getFibonacci(number - 1) + getFibonacci(number - 2);
+        }
+    }
+
+}
