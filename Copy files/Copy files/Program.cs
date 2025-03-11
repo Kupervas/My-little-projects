5fe5da823f9c91aa869f475354c6b155093f81a5
Ruslan <rkupervas2003@gmail.com>
2025-03-11 11:58:44 +0200

    Add files via upload

---
+++ Copy files/Copy files/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введіть шлях до вихідного файлу: ");
+            string sourcePath = Console.ReadLine();
+
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("Вихідний файл не існує ");
+                return;
+            }
+            Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+            string destinationPath = Console.ReadLine();
+
+            File.Copy(sourcePath, destinationPath, true);
+            Console.WriteLine("Файл успішно скопійовано ");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}"); 
+        }
+    }
+}
