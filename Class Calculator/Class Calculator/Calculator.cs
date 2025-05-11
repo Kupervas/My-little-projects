5f4f7f9acc30d83beb484c9b561c1b04b7c2260a
Ruslan <rkupervas2003@gmail.com>
2025-05-11 18:59:43 +0300

    Add files via upload

---
+++ Class Calculator/Class Calculator/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Calculator
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Ділити на нуль не можна");
+            }
+            return x / y;
+
+        }
+    }
+}
---
+++ Class Calculator/Class Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using Class_Calculator;
+using System;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        Console.WriteLine("Введіть перше число");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть операцію: +, -, *, / ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число");
+        double y = Convert.ToDouble(Console.ReadLine());
+        double result = 0;
+        try
+        {
+            switch (operation)
+            {
+                case '+':
+                    result = calc.Add(x, y);
+                    break;
+                case '-':
+                    result = calc.Substract(x, y);
+                    break;
+                case '*':
+                    result = calc.Multiply(x, y);
+                    break;
+                case '/':
+                    result = calc.Divide(x, y);
+                    break;
+            }
+            Console.WriteLine($"Результат: {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+
+    }
+}
