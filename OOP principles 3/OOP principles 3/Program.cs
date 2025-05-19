9110879a16f091120086527e8f70b1d5566fdfde
Ruslan <rkupervas2003@gmail.com>
2025-05-19 20:43:38 +0300

    Add files via upload

---
+++ OOP principles 3/OOP principles 3/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main()
+    {
+        DecimalNumber number = new DecimalNumber(50);
+        number.Show();
+    }
+}
+
+public struct DecimalNumber
+{
+    private int value;
+
+    public int Value
+    {
+        get { return value; }
+        set { this.value = value; }
+    }
+
+    public DecimalNumber(int value)
+    {
+        this.value = value;
+    }
+
+    public string ToBinary() 
+    { 
+        return Convert.ToString(value, 2);
+    }
+    public string ToOctal()
+    {
+        return Convert.ToString(value, 8);
+    }
+
+    public string ToHexadecimal()
+    {
+        return Convert.ToString(value, 16).ToUpper();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine($"Десяткове число: {value}"  );
+        Console.WriteLine($"У двійковій системі: {ToBinary()}" );
+        Console.WriteLine($"У вісімковій системі: {ToOctal()};");
+        Console.WriteLine($"У шістнадцятковій системі: {ToHexadecimal()} ");
+    }
+}
